1c7e6c4fddac0c54ce78a32bbe84b1160465c36c
Borko Tulovic <borkotulovic9991@gmail.com>
2020-12-03 20:16:13 +0100

    Konekcija na bazu podataka, izbacivanje liste svih podataka, unos jednog radnika , izgled forme

--- 46-2018/DataAcessLayer/Models/Salary.cs
+++ 46-2018/DataAcessLayer/Models/Salary.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BuisnessLayer
+namespace DataAcessLayer.Models
 {
-    public class Class1
+   public  class Salary : Person
     {
+        public int Salary { get; set; }
     }
 }

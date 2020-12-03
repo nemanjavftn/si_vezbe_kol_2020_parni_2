d6159ab4f04f9fa14a84817cf1396e877dda9946
Danijela Petric <dpetric16@gmail.com>
2020-12-03 19:51:52 +0100

    Kreiranje DataLayer sloja, Model klase i Repozitorijuma

---
+++ PresentationLayer/DataLayer/EmployeeRepository.cs
@@ -0,0 +1,57 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class EmployeeRepository
+    {
+
+        public List<Employee> GetEmployees()
+        {
+            List<Employee> list = new List<Employee>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Employees";
+
+                sqlConnection.Open();
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    int id = sqlDataReader.GetInt32(0);
+                    string name = sqlDataReader.GetString(1);
+                    string surname = sqlDataReader.GetString(2);
+                    decimal salary = sqlDataReader.GetDecimal(3);
+
+                    Employee e = new Employee(id, name, surname, salary);
+
+                    list.Add(e);
+                }
+                return list;
+            }
+        }
+        public int InsertEmployee(Employee e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =
+                    string.Format("INSERT INTO Employee VALUES('{0}','{1}',{2})", e.Name, e.Surname, e.Salary);
+
+                sqlConnection.Open();
+
+                return sqlCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ PresentationLayer/DataLayer/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public decimal Salary { get; set; }
+
+        public Employee(int id, string name, string surname, decimal salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Surname = surname;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return this.Id + "." + this.Name + " " + this.Surname + " " + this.Salary;
+        }
+    }
+}

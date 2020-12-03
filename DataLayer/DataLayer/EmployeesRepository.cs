350b2279e8219165e528464e2280f63cd645ad12
Strahinja Marenovic <smarenovic@yahoo.com>
2020-12-03 20:04:23 +0100

    Kolokvijum!

---
+++ DataLayer/DataLayer/EmployeesRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+     public class EmployeesRepository
+    {
+        public GetAllEmployees()
+
+        {
+            public List<Employees> GetAllEmployees()
+            {
+                List<Employees> result = new List<Employees>();
+                using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand();
+                    sqlCommand.Connection = sqlConnection;
+                    sqlCommand.CommandText = "SELECT * FROM Students";
+
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Employees S = new Employees();
+                        S.Id = sqlDataReader.GetInt32(0);
+                        S.Name = sqlDataReader.GetString(1);
+                        S.Surname = sqlDataReader.GetString(2);
+                        S.Salary = sqlDataReader.GetDecimal(3);
+                        result.Add(S);
+                    }
+
+                }
+            }
+        }
+    }
+    }
+}
---
+++ dbo.Employees.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [Salary] DECIMAL NOT NULL
+)

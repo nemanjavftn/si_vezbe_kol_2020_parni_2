b6d4f0c0475976a054e7b0c00fba7d1171a6c114
Tanja Prodanovic <pttanja@gmail.com>
2020-12-03 18:26:49 -0100

    Zavrsetak projekta

---
+++ PresentationLayer/DataLayer/EmployeesRepository.cs
@@ -0,0 +1,72 @@
+﻿using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class EmployeesRepository
+    {
+
+        public List<Person> getPerson()
+        {
+            List<Person> pr = new List<Person>();
+            string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CompanyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                string command = "select * from Automobili";
+                SqlCommand com = new SqlCommand(command, con);
+                SqlDataReader dr = com.ExecuteReader();
+                while (dr.Read())
+                    pr.Add(new Person(dr.GetInt32(0),dr.GetString(1), dr.GetString(2), dr.GetDecimal(4)));
+
+            }
+            return pr;
+        }
+        public int insertPerson(Person p)
+        {
+          
+                int result;
+                string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CompanyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                using (SqlConnection con = new SqlConnection(connection))
+                {
+                    con.Open();
+                    string command = "insert into Employees (Name, Surname, Salary) values (@n, @s, @salary)";
+                    SqlCommand com = new SqlCommand(command, con);
+                    com.Parameters.AddWithValue("@n", p.name);
+                    com.Parameters.AddWithValue("@s", p.surname);
+                    com.Parameters.AddWithValue("@salary", p.salary);
+                    
+                    result = com.ExecuteNonQuery();
+                }
+                return result;
+         }
+        public int updatePerson(Person p)
+        {
+            int result;
+            string connection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CompanyDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                con.Open();
+                string commandText = "Update Employees Id=@id, Name=@name, Surname=@surname, Salary=@salary";
+                SqlCommand com = new SqlCommand(commandText, con);
+                com.Parameters.AddWithValue("@name", p.name );
+                com.Parameters.AddWithValue("@id", p.id );
+                com.Parameters.AddWithValue("@surname", p.surname );
+                com.Parameters.AddWithValue("@salary", p.salary);
+
+                result = com.ExecuteNonQuery();
+            }
+            return result;
+        }
+
+    }
+
+
+}
+
+
--- PresentationLayer/PresentationLayer/Form1.Designer.cs
+++ PresentationLayer/PresentationLayer/Form1.Designer.cs
@@ -29,13 +29,125 @@ namespace PresentationLayer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.textBoxSalary = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(464, 213);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(324, 225);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(74, 31);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(74, 77);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSurname.TabIndex = 2;
+            // 
+            // textBoxSalary
+            // 
+            this.textBoxSalary.Location = new System.Drawing.Point(74, 115);
+            this.textBoxSalary.Name = "textBoxSalary";
+            this.textBoxSalary.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSalary.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Salary";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(76, 180);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Unos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(207, 180);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(75, 23);
+            this.buttonRefresh.TabIndex = 8;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxSalary);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Zaposleni";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.TextBox textBoxSalary;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonRefresh;
     }
 }
 

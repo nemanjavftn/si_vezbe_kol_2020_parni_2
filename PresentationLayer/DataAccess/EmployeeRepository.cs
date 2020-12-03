3f50dc94c9e8e375e7f9a4da853146f6aa67af26
R217-12\student <example@gmail.com>
2020-12-03 19:38:36 +0100

    Data sloj

---
+++ PresentationLayer/DataAccess/EmployeeRepository.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class EmployeeRepository
+    {
+        public List<Employee> GetEmployess()
+
+        {
+            List<Employee> employees = new List<Employee>();
+            using (SqlConnection sqlConnection= new SqlConnection(Constants.connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText = "SELECT * FROM Employees";
+
+                SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                while (sqlDataReader.Read())
+                {
+                    Employee e = new Employee();
+                    e.Id = sqlDataReader.GetInt32(0);
+                    e.Name = sqlDataReader.GetString(1);
+                    e.Surname = sqlDataReader.GetString(2);
+                    e.Salary = sqlDataReader.GetDecimal(3);
+                    employees.Add(e);
+                }
+                return employees;
+                    
+               }
+        }
+        public int Insert(Employee e)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(Constants.connectionString))
+            {
+                sqlConnection.Open();
+                SqlCommand sqlCommand = new SqlCommand();
+                sqlCommand.Connection = sqlConnection;
+                sqlCommand.CommandText =string.Format("INSERT INTO Employees VALUES ('{0}','{1}',{2})",e.Name,e.Surname, e.Salary);
+
+                int result=sqlCommand.ExecuteNonQuery();
+                return result;
+            }
+
+        }
+    }
+}

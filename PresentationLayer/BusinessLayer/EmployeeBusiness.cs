148d96ec46d18069b18892306fd1386f0b9fb09e
Nenad Jankovic <nenadjankovic617@gmail.com>
2020-12-03 20:14:41 +0100

    Kreirane metode u Business Layeru

---
+++ PresentationLayer/BusinessLayer/EmployeeBusiness.cs
@@ -0,0 +1,34 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace BusinessLayer
+{   
+    public class EmployeeBusiness
+        
+    {
+        private readonly EmployeeRepository employeeRepositoryl;
+        public EmployeeBusiness()
+        {
+            this.employeeRepositoryl = new EmployeeRepository(); 
+        }
+        public List<Employee> GetAllEmployees(decimal a, decimal b)
+        {
+            return this.employeeRepositoryl.GettAllEmployees().Where(n => n.Salary > a && n.Salary < b).ToList();
+        }
+        public bool insertEmpoyee(Employee s)
+        {
+            if (this.employeeRepositoryl.InsertEmpoyee(s) > 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}

61bc05695b93852a58fe4318658ba7c0ed87d0f3
Danijela Petric <dpetric16@gmail.com>
2020-12-03 20:08:41 +0100

    Kreiranje EmployeeBusiness i metoda

---
+++ PresentationLayer/BusinessLayer/EmployeeBusiness.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer;
+using DataLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+  public class EmployeeBusiness
+    {
+        private readonly EmployeeRepository employeeRepository;
+
+        public EmployeeBusiness()
+        {
+            this.employeeRepository = new EmployeeRepository();
+        }
+        public List<Employee> GetAllEmployees()
+        {
+            return employeeRepository.GetAllEmployees();
+        }
+
+        public List<Employee> getEmployeesSalary(decimal value)
+        {
+            return this.employeeRepository.GetAllEmployees()
+                .Where(e => e.Salary = value).ToList();
+        }
+    }
+}

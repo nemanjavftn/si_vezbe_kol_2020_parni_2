ab1d845199060b842998a51d40db625c6c97f355
Lazar Cubric <cubric.lazar@gmail.com>
2020-12-03 20:12:19 +0100

    Sasa Milinkovic 208-2018

---
+++ BuisnessLayer/DataAcess/EmployeRespiratory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcess
+{
+   public class EmployeRespiratory
+    {
+        public string conncetionEmploye = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LibaryDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Surname] NVARCHAR(50) NOT NULL, 
+    [Salary] DECIMAL NOT NULL
+)

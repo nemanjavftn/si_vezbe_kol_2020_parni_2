f21ff0eda02d5b969a49befcea830077d6f317bf
Nenad Jankovic <nenadjankovic617@gmail.com>
2020-12-03 20:01:22 +0100

    Kreirane funkcije u DataAccessLayer-u

---
+++ PresentationLayer/DataAccessLayer/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public  class Person
+    {
+        private int id;
+        private string name;
+        private string surname;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+    }
+}
